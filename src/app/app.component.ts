e71fe8c59a25198471c47327bd6c7c2635013570
Iôhanes Rocha <iohanesd@gmail.com>
2022-12-03 19:01:53 -0300

    Criando repositório no GitHub

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'projeto00';
+  title = 'Curso de Frontend com Angular';
 }
